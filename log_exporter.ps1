de8f40a71600813c9488f416bc555e77d4301df2
tcp_duckling <107870771+duckduck11@users.noreply.github.com>
2024-03-26 15:37:19 +0100

    Added ps script

---
+++ log_exporter.ps1
@@ -0,0 +1,95 @@
+﻿# API key from https://ipgeolocation.io/
+$API_KEY      = "d4600b4efdef42b39828f5155041a457"
+$LOGFILE_NAME = "failed_rdp.log"
+$LOGFILE_PATH = "C:\ProgramData\$($LOGFILE_NAME)"
+
+# Filter failed RDP events from Windows Event Viewer
+$XMLFilter = @'
+<QueryList> 
+   <Query Id="0" Path="Security">
+         <Select Path="Security">
+              *[System[(EventID='4625')]]
+          </Select>
+    </Query>
+</QueryList> 
+'@
+
+# Infinite Loop that keeps checking the Event Viewer logs.
+while ($true)
+{
+    
+    Start-Sleep -Seconds 1
+    $events = Get-WinEvent -FilterXml $XMLFilter -ErrorAction SilentlyContinue
+    if ($Error) {
+        Write-Host "No Failed Logons found."
+    }
+
+    foreach ($event in $events) {
+
+
+        # $event.properties[19] is the source IP address
+        if ($event.properties[19].Value.Length -ge 5) {
+
+            $timestamp = $event.TimeCreated
+            $year = $event.TimeCreated.Year
+
+            $month = $event.TimeCreated.Month
+            if ("$($event.TimeCreated.Month)".Length -eq 1) {
+                $month = "0$($event.TimeCreated.Month)"
+            }
+
+            $day = $event.TimeCreated.Day
+            if ("$($event.TimeCreated.Day)".Length -eq 1) {
+                $day = "0$($event.TimeCreated.Day)"
+            }
+            
+            $hour = $event.TimeCreated.Hour
+            if ("$($event.TimeCreated.Hour)".Length -eq 1) {
+                $hour = "0$($event.TimeCreated.Hour)"
+            }
+
+            $minute = $event.TimeCreated.Minute
+            if ("$($event.TimeCreated.Minute)".Length -eq 1) {
+                $minute = "0$($event.TimeCreated.Minute)"
+            }
+
+
+            $second = $event.TimeCreated.Second
+            if ("$($event.TimeCreated.Second)".Length -eq 1) {
+                $second = "0$($event.TimeCreated.Second)"
+            }
+
+            $timestamp = "$($year)-$($month)-$($day) $($hour):$($minute):$($second)"
+            $eventId = $event.Id
+            $destinationHost = $event.MachineName
+            $username = $event.properties[5].Value
+            $sourceHost = $event.properties[11].Value
+            $sourceIp = $event.properties[19].Value
+        
+            $log_contents = Get-Content -Path $LOGFILE_PATH
+
+            if (-Not ($log_contents -match "$($timestamp)") -or ($log_contents.Length -eq 0)) {
+            
+                Start-Sleep -Seconds 1
+
+                $API_ENDPOINT = "https://api.ipgeolocation.io/ipgeo?apiKey=$($API_KEY)&ip=$($sourceIp)"
+                $response = Invoke-WebRequest -UseBasicParsing -Uri $API_ENDPOINT
+
+                $responseData = $response.Content | ConvertFrom-Json
+                $latitude = $responseData.latitude
+                $longitude = $responseData.longitude
+                $state_prov = $responseData.state_prov
+                if ($state_prov -eq "") { $state_prov = "null" }
+                $country = $responseData.country_name
+                if ($country -eq "") {$country -eq "null"}
+
+                "latitude:$($latitude),longitude:$($longitude),destinationhost:$($destinationHost),username:$($username),sourcehost:$($sourceIp),state:$($state_prov), country:$($country),label:$($country) - $($sourceIp),timestamp:$($timestamp)" | Out-File $LOGFILE_PATH -Append -Encoding utf8
+
+                Write-Host -BackgroundColor Black -ForegroundColor Magenta "latitude:$($latitude),longitude:$($longitude),destinationhost:$($destinationHost),username:$($username),sourcehost:$($sourceIp),state:$($state_prov),label:$($country) - $($sourceIp),timestamp:$($timestamp)"
+            }
+            else {
+                Write-Host "Event already exists in the custom log. Skipping."
+            }
+        }
+    }
+}
